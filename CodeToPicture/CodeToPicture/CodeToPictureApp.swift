440dc7330da68fe758359aab6e6e803ae74d8323
Anton Paliakou <toxa95401@gmail.com>
2026-03-20 23:10:58 +0100

    Code refactor.

---
+++ CodeToPicture/CodeToPicture/CodeToPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeToPictureApp.swift
+//  CodeToPicture
+//
+//  Created by Anton Paliakov on 20/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeToPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeToPicture/CodeToPictureTests/CodeToPictureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeToPictureTests.swift
+//  CodeToPictureTests
+//
+//  Created by Anton Paliakov on 20/03/2026.
+//
+
+import Testing
+@testable import CodeToPicture
+
+struct CodeToPictureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
